9c6c9b0cfa8a13f08746b91e32bb1f3c0eefe3a2
Klapertart <klapertartinc@gmail.com>
2019-09-22 12:25:32 +0700

    update code

--- src/main/java/com/klapertart/App.java
+++ src/main/java/com/klapertart/App.java
@@ -4,10 +4,6 @@ import com.klapertart.service.ProductService;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- *
- */
 public class App {
     private static SessionFactory factory;
     
@@ -18,12 +14,19 @@ public class App {
             System.err.println("Gagal membuat sessionFactory object." + ex); 
         }
 			
-        //buat service
         ProductService SP = new ProductService(factory);
 
-        //tambah pegawai & mengembalikan id pegawainya
         Integer productID = SP.addProduct("Macbook", 15000000.0, 70);
-        System.out.println("Saved - ProductID : " + productID);
+        
+        SP.listProduct();
+        
+        SP.updateProduct(productID, "Pokcoy");
+        
+        SP.listProduct();
+
+        SP.deleteProduct(productID);
+        
+        SP.listProduct();
         
         factory.close();
     }
--- src/main/java/com/klapertart/service/ProductService.java
+++ src/main/java/com/klapertart/service/ProductService.java
@@ -6,6 +6,8 @@
 package com.klapertart.service;
 
 import com.klapertart.model.Product;
+import java.util.Iterator;
+import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -24,20 +26,74 @@ public class ProductService {
     }
 
     public Integer addProduct(String name, Double price, int stock){
-          Session session = factory.openSession();
-          Transaction tx = null;
-          Integer productID = null;
-          try{
-             tx = session.beginTransaction();
-             Product product = new Product(name, price, stock);
-             productID = (Integer) session.save(product); 
-             tx.commit();
-          }catch (HibernateException e) {
-             if (tx!=null) tx.rollback();
-             e.printStackTrace(); 
-          }finally {
-             session.close(); 
-          }
-          return productID;
-       }    
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer productID = null;
+        try{
+           tx = session.beginTransaction();
+           Product product = new Product(name, price, stock);
+           productID = (Integer) session.save(product); 
+           tx.commit();
+        }catch (HibernateException e) {
+           if (tx!=null) tx.rollback();
+           e.printStackTrace(); 
+        }finally {
+           session.close(); 
+        }
+        return productID;
+    }    
+    
+    public void listProduct(){
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+           tx = session.beginTransaction();
+           // in create query mention class name not table name
+           List<Product> productlis = session.createQuery("FROM Product").list(); 
+           for(Product product : productlis){
+               System.out.println(product.toString());
+           }
+           tx.commit();
+        }catch (HibernateException e) {
+           if (tx!=null) tx.rollback();
+           e.printStackTrace(); 
+        }finally {
+           session.close(); 
+        }
+    }    
+    
+    public void updateProduct(Integer id, String name){
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+           tx = session.beginTransaction();
+           Product product = (Product)session.get(Product.class, id); 
+           product.setName(name);
+           session.update(product); 
+           tx.commit();
+        }catch (HibernateException e) {
+           if (tx!=null) tx.rollback();
+           e.printStackTrace(); 
+        }finally {
+           session.close(); 
+        }
+    }    
+    
+    
+    public void deleteProduct(Integer id){
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+           tx = session.beginTransaction();
+           Product product = (Product)session.get(Product.class, id); 
+           session.delete(product); 
+           tx.commit();
+        }catch (HibernateException e) {
+           if (tx!=null) tx.rollback();
+           e.printStackTrace(); 
+        }finally {
+           session.close(); 
+        }
+    }
+        
 }
